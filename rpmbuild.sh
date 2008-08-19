ce6a977d2c99ffeb43bcedd546552e636e8263cf
Arnaldo Carvalho de Melo <acme@redhat.com>
2008-08-19 16:24:30 -0300

    rpm: Use git-archive to create tarball
    Use git-archive to create tarball
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- rpmbuild.sh
+++ rpmbuild.sh
@@ -9,9 +9,22 @@ mkdir RPM/root
 rpm=`pwd`/RPM
 src=`pwd`
 
+pkg=syspart
+
+if [ ! -e $pkg.spec ] ; then
+	echo "run ./bootstrap first"
+	exit 1
+fi
+
+ver=$(rpm -q --qf '%{version} ' --specfile $pkg.spec | cut -d' ' -f1)
+
+git archive --format=tar --prefix=$pkg-$ver/ HEAD | \
+        gzip -9 > $src/$pkg-$ver.tar.gz
+
+
 rpmbuild --buildroot="$rpm/root"         \
 	 --define="_rpmdir $rpm/pkg"     \
 	 --define="_builddir $rpm/build" \
 	 --define="_sourcedir $src"   \
 	 --define='_unpackaged_files_terminate_build 0' \
-	 -bb syspart.spec
+	 -bb $pkg.spec
