2c5bb202b809a539ec4e902218e970d903932ee0
Jon Miller <jsmiller@qti.qualcomm.com>
2013-02-05 10:20:43 -0800

    Making rpmbuild.sh more automated by running ./bootstrap and ./configure itself

--- rpmbuild.sh
+++ rpmbuild.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+# Everything is relative to the working directory
+cd $(dirname $0)
+FD=$(pwd)
+
 rm -rf RPM
 mkdir RPM
 mkdir RPM/build
@@ -12,8 +16,10 @@ src=`pwd`
 pkg=syspart
 
 if [ ! -e $pkg.spec ] ; then
-	echo "run ./bootstrap first"
-	exit 1
+	echo "Running ./bootstrap first"
+	./bootstrap || { echo "Error: ${FD}/bootstrap errored. Quiting."; exit 1; }
+	echo "Now running ./configure"
+	./configure || { echo "Error: ${FD}/configure failed. See config.log for details."; exit 2; }
 fi
 
 ver=$(rpm -q --qf '%{version} ' --specfile $pkg.spec | cut -d' ' -f1)
