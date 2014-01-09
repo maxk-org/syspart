510491c442854bf38d30010908d894faa70ea261
Max Krasnyansky <maxk@qti.qualcomm.com>
2014-01-08 21:41:03 -0800

    Build improvements
    Switched to flat makefiles.
    Added support for generating version based on git tags.
    Simplified RPM building.

---
+++ acversion.mk
@@ -0,0 +1,6 @@
+$(top_srcdir)/version.mk: force-version-check
+	@$(top_srcdir)/generate-version $@
+
+force-version-check:
+
+-include $(top_srcdir)/version.mk
--- rpmbuild.sh
+++ rpmbuild.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Everything is relative to the working directory
-cd $(dirname $0)
-FD=$(pwd)
-
-rm -rf RPM
-mkdir RPM
-mkdir RPM/build
-mkdir RPM/pkg
-mkdir RPM/root
-
-rpm=`pwd`/RPM
-src=`pwd`
-
 pkg=syspart
 
 if [ ! -e $pkg.spec ] ; then
@@ -22,15 +9,4 @@ if [ ! -e $pkg.spec ] ; then
 	./configure || { echo "Error: ${FD}/configure failed. See config.log for details."; exit 2; }
 fi
 
-ver=$(rpm -q --qf '%{version} ' --specfile $pkg.spec | cut -d' ' -f1)
-
-git archive --format=tar --prefix=$pkg-$ver/ HEAD | \
-        gzip -9 > $src/$pkg-$ver.tar.gz
-
-
-rpmbuild --buildroot="$rpm/root"         \
-	 --define="_rpmdir $rpm/pkg"     \
-	 --define="_builddir $rpm/build" \
-	 --define="_sourcedir $src"   \
-	 --define='_unpackaged_files_terminate_build 0' \
-	 -bb $pkg.spec
+make rpm
